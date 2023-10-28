2d1f40edccf8842bebb272b31b81c9e1f50c7064
Damianos Giankakis <damgiankakis@gmail.com>
2023-10-28 12:22:12 +0300

    Adds dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { resolve } from "path";
 
+/** @type {import('vite').UserConfig} */
 export default {
   resolve: {
     alias: {
       "@three/examples": resolve(__dirname, "node_modules/three/examples/jsm"),
     },
+  },
+  build: {
+    outDir: "../dist"
   },
 };
